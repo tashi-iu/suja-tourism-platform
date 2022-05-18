0f3b41668e5118ca78d41a5808eb1e736ed52545
Tashi Tobgay Dakpa <tashitobgaylibra@gmail.com>
2022-05-18 13:21:58 +0600

    add social layout route

--- app/routes/__social.tsx
+++ app/routes/__social.tsx
@@ -1,58 +1,75 @@
-import { Outlet } from "@remix-run/react";
 import {
+  Outlet,
+  useFetcher,
+  useNavigate,
+  useTransition,
+} from "@remix-run/react";
+import {
+  BiBell,
   BiCalendarEvent,
   BiChat,
+  BiChevronDown,
   BiCog,
   BiGroup,
   BiHomeCircle,
   BiSearchAlt2,
-  BiUser
+  BiUser,
 } from "react-icons/bi";
 import { FaGoogle } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 import { MdTour } from "react-icons/md";
-import { VscBell } from "react-icons/vsc";
-import NavBar from "~/components/NavBar";
-import ProviderSignInButton from "~/components/ProviderSignInButton";
-import Avatar from "~/components/ui/Avatar";
-import SearchInput from "~/components/ui/SearchInput";
-import { useOptionalUser } from "~/utils";
+import ProviderSignInButton from "~/components/auth/ProviderSignInButton";
+import Avatar from "~/components/ui-kit/Avatar";
+import DropdownMenu from "~/components/ui-kit/DropdownMenu";
+import NavBar from "~/components/ui-kit/NavBar";
+import SearchInput from "~/components/ui-kit/SearchInput";
+import { useOptionalProfile, useOptionalUser } from "~/utils";
 
 export default function SocialLayout() {
+  const transition = useTransition();
+  const navigate = useNavigate();
+  const fetcher = useFetcher();
   const user = useOptionalUser();
+
+  const profile = useOptionalProfile();
+  const hasUser = user?.role === "authenticated" && profile;
+
   const notifications: any[] = [];
   const friends: any[] = [];
-  const hasUser = !!user?.id;
+
   return (
     <div className="flex">
-      <div className="invisible flex h-screen w-1/5 flex-col gap-y-4 bg-slate-200/50 p-4 md:visible">
-        <div>
-          <img src="assets/images/logo.png" alt="Suja" className="w-32" />
-        </div>
-        <SearchInput placeholder="Search" />
-        {hasUser ? (
-          <div className="mt-4">
-            <div>
-              {friends.length ? (
-                friends.map((friend) => (
-                  <div
-                    key={friend.id}
-                    className="flex items-center justify-between gap-x-2"
-                  >
-                    <div className="flex gap-x-2">
-                      <Avatar src="" alt="Profile picture" size={18} />
-                      <p className="flex-1 text-ellipsis text-slate-700/80">
-                        Some friend
-                      </p>
-                    </div>
-                    <span className="h-2 w-2 rounded-full bg-emerald-500/60" />
-                  </div>
-                ))
-              ) : (
-                <p>Add a friend.</p>
-              )}
-            </div>
+      <div className="invisible flex w-1/4 items-stretch md:visible">
+        <div className="flex w-full flex-col gap-y-4 p-4">
+          <div>
+            <img src="assets/images/logo.png" alt="Suja" className="w-28" />
           </div>
-        ) : null}
+          <SearchInput placeholder="Search" />
+          {hasUser ? (
+            <div className="mt-4">
+              <div>
+                {friends.length ? (
+                  friends.map((friend) => (
+                    <div
+                      key={friend.id}
+                      className="flex items-center justify-between gap-x-2"
+                    >
+                      <div className="flex gap-x-2">
+                        <Avatar src="" alt="Profile picture" size={28} />
+                        <p className="flex-1 text-ellipsis text-slate-700/80">
+                          Some friend
+                        </p>
+                      </div>
+                      <span className="h-2 w-2 rounded-full bg-emerald-500/60" />
+                    </div>
+                  ))
+                ) : (
+                  <p>Add a friend.</p>
+                )}
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
       <div className="flex flex-1 flex-col">
         <div className="flex flex-col">
@@ -100,7 +117,7 @@ export default function SocialLayout() {
                 {
                   icon: BiUser,
                   key: "profile",
-                  name: "Your Profile",
+                  name: "My Profile",
                   to: `/u/${user?.id}`,
                   hidden: !hasUser,
                 },
@@ -115,29 +132,59 @@ export default function SocialLayout() {
             />
           </div>
           <div>
-            <Outlet />
+            {transition.state === "loading" ? <div>Loading</div> : <Outlet />}
           </div>
         </div>
       </div>
-      <div className="invisible flex h-screen w-1/5 flex-col gap-4 bg-slate-200/50 p-4 md:visible">
-        {hasUser ? (
-          <div className="flex justify-end">
-            <div className="relative">
-              {notifications.length ? (
-                <span className="absolute top-0 right-0 h-2 w-2 rounded-full bg-emerald-500" />
-              ) : null}
-              <VscBell size="18" className="text-slate-500/80" />
+      <div className="invisible flex w-1/4 items-stretch md:visible">
+        <div className=" flex w-full flex-col gap-4 p-4">
+          {hasUser ? (
+            <div className="flex items-center justify-between">
+              <div className="relative">
+                {notifications.length ? (
+                  <span className="absolute top-0 right-0 h-2 w-2 rounded-full bg-emerald-500" />
+                ) : null}
+                <button className="rounded-full p-2 hover:bg-slate-200/80">
+                  <BiBell size="18" className="text-slate-500/80" />
+                </button>
+              </div>
+              <DropdownMenu
+                items={[
+                  {
+                    key: "my-profile",
+                    icon: BiUser,
+                    onClick: () => {
+                      navigate(`/u/${profile.id}`);
+                    },
+                    label: "My Profile",
+                  },
+                  {
+                    key: "logout",
+                    icon: FiLogOut,
+                    onClick: () => {
+                      const formData = new FormData();
+                      fetcher.submit(formData, {
+                        action: "/api/sign-out",
+                        method: "post",
+                        replace: true,
+                      });
+                    },
+                    label: "Log Out",
+                  },
+                ]}
+              >
+                <Avatar src={profile.avatar_url} alt="Profile" size={28} />
+                <p className="text-white/80">{profile.name}</p>
+                <BiChevronDown size="18" className="text-white/80" />
+              </DropdownMenu>
             </div>
-            <div className="flex justify-end">
-              <p>{user?.user_metadata.full_name}</p>
-            </div>
-          </div>
-        ) : (
-          <ProviderSignInButton provider="google">
-            <FaGoogle className="h-4 w-4" />
-            <div className="leading-none">Sign in with Google</div>
-          </ProviderSignInButton>
-        )}
+          ) : (
+            <ProviderSignInButton provider="google">
+              <FaGoogle className="h-4 w-4" />
+              <div className="leading-none">Sign in with Google</div>
+            </ProviderSignInButton>
+          )}
+        </div>
       </div>
     </div>
   );
